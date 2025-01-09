1af04a4d0e2d28a722d6e85a84cc7675a12eb504
toproof <kmnlmn123@gmail.com>
2025-01-09 14:49:16 +0900

    업로드

---
+++ test/test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() 
+{
+  int a = 5;
+  printf("Gdgd %d", a);
+
+  if (a == 1)
+  {
+    printf("Gdgd %d", a);
+  }
+  else
+  {
+    a = a + 50;
+    printf("Gdgd %d", a + 55);
+  }
+
+  return 0;
+}
