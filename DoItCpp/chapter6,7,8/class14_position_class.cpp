e3136e9be19c14fff1d4b792196408d74c8e4f9a
toproof <kmnlmn123gmail.com>
2025-02-12 18:15:56 +0900

    추상 클래스를 이용하여 position class 제작 학습

--- DoItCpp/chapter6,7,8/class14_position_class.cpp
+++ DoItCpp/chapter6,7,8/class14_position_class.cpp
@@ -1,21 +1,15 @@
 /*
-* 트랜스폼 클래스 제작해보기
 
-Transform class : 컴포지션 포지션, 크기, 회전
+포지션 클래스 제작해보기
 
 추상 클래스 (좌표 함수들)
 - 좌표 리셋 순수 가상 함수
 - 좌표 설정 순수 가상 함수
 - 좌표 출력 순수 가상 함수
-- 연산자 오버로딩 vector3 반환 +, -
 
 포지션 class : 추상 클래스를 상속
-
-
-회전 class : 추상 클래스를 컴포지션으로 사용
-
-vector3 클래스
-- x, y, z값
+- 연산자 오버로딩
+- x, y, z 부동 소수점 좌표
 
 */
 
@@ -23,13 +17,14 @@ vector3 클래스
 #include <windows.h>
 using namespace std;
 
-// position, rotation, scale 클래스의 바탕이되는 추상 클래스
+// position, rotation 추상 클래스
 class coordinate
 {
   public:
+    // 순수 가상 함수를 선언
     virtual void reset_vector()=0;
     virtual void set_vector(double x, double y, double z)=0;
-    virtual void print_vector()=0;
+    virtual void print_vector() const =0;
 };
 
 // 추상 클래스 상속을 이용한 position 클래스
@@ -41,7 +36,7 @@ class position : public coordinate
 
     virtual void reset_vector() override;
     virtual void set_vector(double x, double y, double z) override;
-    virtual void print_vector() override;
+    virtual void print_vector() const override;
 
     position operator+(const position &vector);
     position operator-(const position &vector);
@@ -52,9 +47,9 @@ class position : public coordinate
 
 void position::reset_vector()
 {
-  this->x = 0.0;
-  this->y = 0.0;
-  this->z = 0.0;
+  this->x = 0;
+  this->y = 0;
+  this->z = 0;
 }
 void position::set_vector(double x, double y, double z)
 {
@@ -62,9 +57,9 @@ void position::set_vector(double x, double y, double z)
   this->y = y;
   this->z = z;
 }
-void position::print_vector()
+void position::print_vector() const
 {
-  cout << "좌표 ("<< this->x << ", " << this->y << ", " << this->z << ")" << endl;
+  cout << "포지션 좌표 출력 (" << this->x << ", " << this->y << ", " << this->z << ")" << endl;
 }
 
 position position::operator+(const position &pos) 
@@ -78,16 +73,23 @@ position position::operator-(const position &pos)
 
 int main()
 {
-  position p;
-  p.print_vector();
-  p.set_vector(5.2, 12.1, 4.666);
-  p.print_vector();
-  p.reset_vector();
-  p.print_vector();
-
-
+  position p1;
+  p1.print_vector();
+  p1.set_vector(5.2, 12.1, 4.666);
+  p1.print_vector();
 
   std::cout << "\n\n";
+
+  position p2;
+  p2.print_vector();
+  p2.set_vector(1115.2, 1112.1, 4111.666);
+  p2.print_vector();
+
+  std::cout << "\n\n";
+
+  position p3 = p1 + p2;
+  p3.print_vector();
+
   system("pause");
   return 0;
 }
