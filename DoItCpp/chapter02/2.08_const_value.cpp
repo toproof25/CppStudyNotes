01c0e55846c0b5570c133d463a578533b9b3923e
toproof <kmnlmn123@gmail.com>
2025-06-17 15:07:35 +0900

    📚 Study: 상수 const 학습

--- DoItCpp/chapter02/2.08_const_value.cpp
+++ DoItCpp/chapter02/2.08_const_value.cpp
@@ -2,7 +2,6 @@
 * 상수 선언 및 사용
 */
 #include <iostream>
-#include <windows.h>
 
 int main()
 {
@@ -11,10 +10,8 @@ int main()
     std::cout << "PI : " << PI << std::endl;
 
     // const로 선언된 변수는 수정이 불가하며, 오류가 발생
-    // 식이 수정할 수 있는 lvalue여야 합니다.
+    // 식이 수정할 수 있는 lvalue여야 한다
     //PI = 3.1415926535;
 
-    std::cout << "\n\n";
-    system("pause");
     return 0;
 }
