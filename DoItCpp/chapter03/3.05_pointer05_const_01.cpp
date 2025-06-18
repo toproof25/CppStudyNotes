7e5ff195a12155013cbf93fc0065ebc4a50823aa
toproof <kmnlmn123@gmail.com>
2025-06-18 10:03:39 +0900

    📚 Study: Chapter03 - const와 포인터 (값 상수)

--- DoItCpp/chapter03/3.05_pointer05_const_01.cpp
+++ DoItCpp/chapter03/3.05_pointer05_const_01.cpp
@@ -16,7 +16,7 @@ int main()
     // 포인터 변수가 가리키는 값을 상수화 했기 때문에 값을 변경할 수 없음
     //*p_number1 = 9999;
 
-    // 하지만 number1 자체는 상수화가 되어 있지 않아 값 변경이 가능
+    // 하지만 number1 자체는 상수화가 되어 있지 않아 값 변경이 가능하다
     number1 = 20;
 
     std::cout << "number1: " << number1 << std::endl;
