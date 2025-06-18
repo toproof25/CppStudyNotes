83fbd99f27976aa73fc45acee3ada430f79d3e82
toproof <kmnlmn123@gmail.com>
2025-06-18 13:50:35 +0900

    📚 Study: Chapter04 - 조건문 if문, switch문

--- DoItCpp/chapter04/4.01_Conditional.cpp
+++ DoItCpp/chapter04/4.01_Conditional.cpp
@@ -1,5 +1,5 @@
 /*
-* CPP 조건문
+* CPP 조건문, if문과 switch문 학습
 */
 #include <iostream>
 using namespace std;
@@ -7,9 +7,11 @@ using namespace std;
 int main()
 {
   int number = 10;
+  char c = 'B';
 
   // 조건이 참인 경우를 순차적으로 비교하여 실행
-  if (number > 0)
+  // 단락 평가를 하여 앞선 조건에 따라 결과가 정해지면 결과적으로 참과 거짓을 결정함
+  if (number > 0 && c == 'A')
   {
     cout << "number는 양수입니다" << endl; 
   }
@@ -23,7 +25,7 @@ int main()
   }
 
   // 어떤 값에 따라 점프 테이블 형태로 실행
-  char c = 'B';
+  // if문은 참일 때 까지 순차적으로 모든 if~else if를 검사하나, switch는 표현식에 따라 바로 해당 case를 실행
   switch (c)
   {
     case 'A':
