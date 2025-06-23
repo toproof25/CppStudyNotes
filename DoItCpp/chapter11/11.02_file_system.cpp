0f1c7fd3acbbd6e8348c8adaf9d289442d04217a
toproof <kmnlmn123@gmail.com>
2025-06-23 12:46:44 +0900

    📚 Study: Chapter11 - filesystem을 이용한 파일 기능 사용

--- DoItCpp/chapter11/11.02_file_system.cpp
+++ DoItCpp/chapter11/11.02_file_system.cpp
@@ -1,5 +1,6 @@
 /*
 C++ 17 버전 이상으로 맞추고 해야 정상적으로 동작
+filesystem을 이용하여 디렉토리 생성, 텍스트 파일 생성 및 수정
 */
 #include <iostream>
 #include <string>
@@ -14,6 +15,7 @@ int main()
   // 절대 경로로 설정
   string path = "E:\\CppStudyNotes\\DoItCpp\\chapter11\\MyDirectory";
   fs::create_directories(path);
+
 
   ofstream outFile(path + "/test.txt");
   outFile << "Hello, File System!" << endl;
