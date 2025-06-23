3a054dfbbf5c95b596adadb5e7b5175cbe813a89
toproof <kmnlmn123@gmail.com>
2025-06-23 15:50:49 +0900

    📚 Study: Chapter11 - filesystem을 이용한 파일 기능 사용

--- DoItCpp/chapter11/11.02_file_system.cpp
+++ DoItCpp/chapter11/11.02_file_system.cpp
@@ -16,12 +16,41 @@ int main()
   string path = "E:\\CppStudyNotes\\DoItCpp\\chapter11\\MyDirectory";
   fs::create_directories(path);
 
-
+  // test.txt 파일 생성
   ofstream outFile(path + "/test.txt");
   outFile << "Hello, File System!" << endl;
   outFile.close();
+  cout << "생성 완료 > 경로 : " + path  << endl;
+  cout << endl;
 
-  cout << "생성 완료" << endl;
+
+  // 디렉토리 내 파일 확인
+  // fs::directory_interator : 반복을 위한 반복자
+  // fs::directory_entry : 디렉토리 내 항목에 대한 각 클래스
+  cout << "파일 확인하기"  << endl;
+  for (const fs::directory_entry &entry : fs::directory_iterator(path))
+  {
+    // 경로에 해당하는 것이 파일인지 확인
+    if (entry.is_regular_file())
+    {
+      cout << entry.path().filename() << endl;
+    }
+  }
+  cout << endl;
+
+  // 파일 읽기
+  cout << "파일 읽기"  << endl;
+  ifstream inFile(path+"/test.txt");
+  string line;
+  while (getline(inFile, line))
+  {
+    cout << line << endl;
+  }
+  inFile.close();
+  cout << endl;
+
+
+  fs::remove_all(path);
 
   
   return 0;
