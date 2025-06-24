e41f27c2e1fbee0d9c338090d252980a384298bc
toproof <kmnlmn123@gmail.com>
2025-06-24 11:45:26 +0900

    📚 Study: Chapter12 - vector 동적 배열 컨테이너 학습

--- DoItCpp/chapter12/12.03_vector.cpp
+++ DoItCpp/chapter12/12.03_vector.cpp
@@ -13,6 +13,17 @@ insert(), erase() - 반복자로 중간에 값을 삽입 및 제거
 #include <stdexcept>
 using namespace std;
 
+// template으로 어떤 자료형의 vector라도 출력할 수 있도록 함수 구현
+template<typename T>
+void print_vector_all(const vector<T>& v)
+{
+  for(const T& i : v)
+  {
+    cout << "v : " << i << endl;
+  }
+}
+
+
 int main()
 {
   // vector 정의
@@ -35,11 +46,7 @@ int main()
     v2.push_back(i);
 
 
-  // 원소 추가 후 출력
-  for (it = v2.begin(); it != v2.end(); ++it)
-  {
-    cout << "원소가 [추가된] v2의 값 : " << *it << endl;
-  }
+  print_vector_all(v2);
   cout << endl;
 
 
@@ -69,7 +76,7 @@ int main()
   }
   cout << endl;
 
-  // erase(반복자, 값) - 반복자 앞으로 값을 추가
+  // erase(반복자) 해당 위치의 요소를 제거
   v2.erase(v2.begin()+2); // v2[2] 앞에 25 추가
 
   // 먼저 v2.begin()+2 가 제어되었기 때문에 반복자가 앞당겨져서 의도하지 않은 값이 사라질 수 있다
@@ -83,13 +90,20 @@ int main()
   cout << endl;
 
 
-  // 정적 반복자 - 반복자가 가리키는 원소의 값을 변경할 수 없다
+  // 상수 반복자 - 반복자가 가리키는 원소의 값을 변경할 수 없다
   // 증감 연산자로 역참조를 하는 등의 연산은 가능
   vector<int>::const_iterator cit = v2.cbegin();
   cout << "++cit : " << *(++cit) << endl;
   cout << endl;
   
 
+
+  // 리버스 반복자 - rbegin은 원소의 마지막, rend는 첫원소의 이전 주소
+  for (vector<int>::reverse_iterator rit = v2.rbegin(); rit != v2.rend(); rit++)
+  {
+    cout << "리버스 반복자 : " << *rit << endl;
+  }
+  cout << endl;
 
 
 
