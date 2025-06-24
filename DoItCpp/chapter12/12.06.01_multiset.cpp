ff31212f85c303b96ac835efa4a77c5917dee1c2
toproof <kmnlmn123@gmail.com>
2025-06-24 15:48:30 +0900

    📚 Study: Chapter12 - multiset 이해와 학습

--- DoItCpp/chapter12/12.06.01_multiset.cpp
+++ DoItCpp/chapter12/12.06.01_multiset.cpp
@@ -1,6 +1,5 @@
 /*
-multiset은 중복을 허용하는 set
-
+multiset은 중복을 허용하면서 정렬된 집합으로 데이터를 관리하는 컨테이너이다
 */
 #include <iostream>
 #include <set>
@@ -24,13 +23,24 @@ int main()
   multiset<string> mySet = {"김철수", "김짱구"}; // 유니폼 초기화
 
   // multiset.insert(값) - multiset에 데이터를 추가한다
-  // multiset은 중복값을 허용하기에 기존 "김철수", "김짱구"가 있어도 원소가 추가된다
-  mySet.insert("김철수");
-  mySet.insert("김유리");
+  // 중복 데이터를 허용
   mySet.insert("김짱구");
   mySet.insert("신짱구");
   mySet.insert("박명수");
+  mySet.insert("김유리");
+  mySet.insert("김유리");
+  mySet.insert("김유리");
+  mySet.insert("김유리");
+  mySet.insert("김유리");
+
+  // multiset의 insert의 반환은 set<string>::iterator 으로 interator는 삽인된 원소 반복자
+  multiset<string>::iterator result = mySet.insert("김철수");
+  cout << "["+(*result)+"]를 추가하였습니다" << endl;
+
   print_set_all(mySet);
+
+
+
 
   // multiset.find(찾을값) - 원소를 찾고, 원소가 없으면 end()를 반환
   string find_value = "김철수";
@@ -49,12 +59,35 @@ int main()
   cout << endl;
 
 
+
+
   // multiset.size() - multiset 사이즈 구하기
   cout << "mySet 사이즈 : " << mySet.size() << endl << endl;
 
-  // erase(제거할 값)
-  mySet.erase("김철수");
-  mySet.erase("신짱구");
+
+
+
+  // erase(제거할 값) - 부호 없는 정수가 반환되어 확인 가능
+  // set과 차이점은 multiset의 경우 제거할 원소를 모두 제거한다 
+  // (즉 "김유리"가 중복으로 5개가 있으면 5개가 모두 제거된다)
+  cout << "mySet.count(""김유리"") : " << mySet.count("김유리") << endl;
+
+  // iterator로 erase를 하면 원소 하나만 제거된다
+  multiset<string>::iterator find_result = mySet.find("김유리");
+  mySet.erase(find_result);
+  cout << "erase에 iterator로 제거 (원소 하나만 제거).... ";
+  cout << "mySet.count(""김유리"") : " << mySet.count("김유리") << endl;
+  
+  
+  cout << "김유리 데이터로 모두 제거.....";
+  int erase_result = mySet.erase("김유리");
+  if (erase_result)
+    cout << "김유리가 제거되었습니다" << endl;
+
+  erase_result = mySet.erase("김철수수수수수수");
+  if (!erase_result)
+    cout << "김철수수수수수수는 원소에 존재하지 않아 제거 실패" << endl;
+
   cout << "erase 후 mySet 사이즈 : " << mySet.size() << endl;
   print_set_all(mySet); 
 
