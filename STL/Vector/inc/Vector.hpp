29fbf7796209e5621c610562304b28ba85557a2a
toproof <kmnlmn123@gmail.com>
2026-01-14 13:56:50 +0900

    feat: Vector 클래스 개선 및 주석 추가
    - JavaDoc 스타일 주석 추가 및 수정
    - reserve와 resize 함수 내에서 타입 확인 후 복사/이동 구현
    - 수동(반복문)으로 메모리 공간에 요소를 복사/이동하는 작업을 uninitialized_copy/uninitialized_move로 변경
    - 수동(반복문)으로 기존 메모리 공간 요소를 제거하는 작업을 destroy로 변경
    - insert, emplace, erase 함수에 추가 위치가 end()인 경우 기존 push, pop과 동일하게 동작하도록 수정
    - 오타 수정

--- STL/Vector/inc/Vector.hpp
+++ STL/Vector/inc/Vector.hpp
@@ -1,20 +1,26 @@
 /**
  * @file Vector.hpp
- * @date 2026-01-13
+ * @date 2026-01-14
  * @author toproof (kmnlmn123@gmail.com)
- * @brief Raw Memory Vector Class 구현
+ * @brief Raw Memory를 이용하여 동적 크기 Vector Class 구현
+ * @details 
+ * 1. allocator를 이용하여 메모리 공간을 할당/재할당
+ * 2. Placement New를 이용하여 메모리 공간에 객체를 생성
+ * 3. 메모리 재할당에서 예외 안전성을 구현
  */
 
-#pragma ones
+#pragma once
 
 #include <memory>
 #include <new>
 #include <iostream>
 #include <utility>
+#include <type_traits>
+#include <stdexcept>
 
 
 /**
- * @brief Raw Memory Vector Class
+ * @brief Raw Memory로 객체를 관리하는 동적 Vector Class
  * @tparam T Vector가 담을 요소의 타입
  */
 template <typename T>
@@ -25,9 +31,9 @@ class Vector
 
   private:
     size_t _size = 0;
-    size_t _capacity = 0;
-    T* vectorArray = nullptr;
-    std::allocator<T> alloc;
+    size_t _capacity = 0;      
+    T* vectorArray = nullptr;  ///< 실제 요소들이 저장된 곳을 가리키는 배열 포인터
+    std::allocator<T> alloc;   
 
   private:
     bool isFull() { return (_size == _capacity); }
@@ -35,20 +41,15 @@ class Vector
     void resize();
 
   public:
-    /**
-     * @brief 기본 생성자
-     * @note 기본 크기 변수들 초기화
-     */
+
+    /** @brief 기본 변수값을 초기화하는 기본 생성자 */
     Vector() noexcept : _size(0), _capacity(0), vectorArray(nullptr) {}
 
-    /**
-     * @brief 소멸자
-     * @note 모든 요소와 메모리 공간을 회수
-     */
+    /** @brief Vector 내 모든 요소를 소멸하고 메모리 공간을 회수하는 소멸자 */
     ~Vector();
 
     /**
-     * @brief 복사 생성자
+     * @brief 모든 요소를 복사하는 복사 생성자
      * @param other 복사될 Vector<T> 객체
      * @note 
      * 1. other의 크기를 복사
@@ -57,68 +58,45 @@ class Vector
     Vector(const Vector<T>& other);
 
     /**
-     * @brief 이동 생성자
-     * @param other 이동할 R-value other 객체
-     * @note other의 메모리 공간을 그대로 이전
+     * @brief 요소의 소유권을 이전하는 이동 생성자
+     * @param other 소유권을 이전할 R-value로 이동 후 사용 불가한 객체
+     * @note R-value의 소유권 이전으로 메모리 비용을 줄이고 효율적으로 이동하기 위해 작성
      */
     Vector(Vector<T>&& other) noexcept;
 
     /**
-     * @brief 복사 대입 생성자
-     * @param other 대입 연산자의 우측값
+     * @brief Copy and Swap 방식으로 복사해오는 생성자
+     * @param other 대입 연산자의 우측값으로 원본에 영향은 없습니다
      * @return 체인 작용을 위한 자신을 반환
-     * @note 복사된 other의 내부 변수를 swap하여 이동
+     * @note Copy and Swap 방식으로 내부 변수를 swap하여 이동합니다
      */
     Vector<T>& operator=(Vector<T> other);
 
-    /**
-     * @brief [] 연산자 오버라이드
-     * @param index 반환할 요소의 특정 위치
-     * @return index 위치의 참조 값
-     * @warning 범위에 대한 예외처리를 하지 않으므로 범위에 주의
-     * @note index위치의 참조 값을 반환하여 수정, 읽기 전용 오버로딩
-     */
+
     T& operator[](size_t index) noexcept { return vectorArray[index]; }
     const T& operator[](size_t index) const noexcept { return vectorArray[index]; }
 
-    /**
-     * @brief index 위치 요소를 참조 반환
-     * @param index 반환할 요소의 특정 위치
-     * @return index 위치의 참조 값
-     * @throw out_of_range (index 범위에 따라 반환)
-     * @note index위치의 참조 값을 반환하여 수정, 읽기 전용 오버로딩
-     */
     T& at(size_t index);
     const T& at(size_t index) const;
 
-    /**
-     * @brief 첫번째 요소를 반환하는 함수입니다
-     * @return vector 객체의 첫번째 요소를 반환합니다
-     * @throw out_of_range (빈 공간에서 호출 시 예외 발생)
-     * @note 수정과 읽기 변수로 오버로딩
-     */
     T& front() { return vectorArray[0]; }
     const T& front() const { return vectorArray[0]; }
 
-    /**
-     * @brief 마지막 요소를 반환하는 함수입니다
-     * @return vector 객체의 마지막 요소를 반환합니다
-     * @throw out_of_range (빈 공간에서 호출 시 예외 발생)
-     * @note 수정과 읽기 변수로 오버로딩
-     */
     T& back() { return vectorArray[_size-1]; }
     const T& back() const { return vectorArray[_size-1]; }
 
     /**
-     * @brief 객체의 크기를 미리 할당하는 함수
-     * @param _capacity 할당할 크기
+     * @brief 메모리 재할당을 방지하기 위해 vector 메모리 크기를 미리 할당하는 함수입니다
+     * @param newCapacity 할당할 메모리 크기 (capacity보다 작은 값은 무시합니다)
+     * @throw 메모리 복사/이동 중 오류 발생 시 예외가 발생하며, 생성한 메모리 공간은 회수됩니다
+     * @note T타입의 이동 생성자 예외 유무에 따라 복사 혹은 이동을 수행합니다
      */
-    void reserve(size_t _capacity);
+    void reserve(size_t newCapacity);
 
     /**
-     * @brief vector 객체의 마지막에 요소를 추가하는 함수
-     * @param value 추가할 요소의 값 (복사)
-     * @warning 크기를 초과 시 재할당이 발생하고 O(n)의 시간이 소요될 수 있음
+     * @brief vector 마지막 위치에 요소를 추가하는 함수
+     * @param value 추가할 요소의 값 (L-value값은 복사, R-value값은 이동)
+     * @note 크기를 초과 시 재할당이 발생하고 O(n)의 시간이 소요될 수 있음
      */
     void push_back(const T& value);
     void push_back(T&& value);
@@ -126,39 +104,52 @@ class Vector
     /**
      * @brief vector 객체의 마지막 요소를 제거하는 함수
      * @throw out_of_range (빈 공간에서 호출 시 예외 발생)
+     * @warning vector 내 남은 요소를 확인한 후 호출
      */
     void pop_back();
 
     /**
-     * @brief vector 객체의 마지막에 요소를 생성자 인자로 추가하는 함수
+     * @brief vector 객체의 마지막 위치에 생성자 인자로 객체를 생성하여 추가하는 함수
      * @param args 추가할 요소의 생성자 인자들
-     * @warning 크기를 초과 시 재할당이 발생하고 O(n)의 시간이 소요될 수 있음
+     * @note 크기를 초과 시 재할당이 발생하고 O(n)의 시간이 소요될 수 있음
      */
     template <typename ...Args> 
     void emplace_back(Args&&... args);
 
     /**
      * @brief capacit를 size에 맞게 줄입니다
-     * @note 메모리 재할당을 이용하여 capacity를 size에 맞춤
+     * @note size만큼 새로운 메모리에 재할당하여 capacity를 size에 맞춥니다
      */
     void shrink_to_fit();
 
-    /**
-     * @brief 모든 요소를 제거합니다
-     * @note 모든 요소를 소멸하고 size가 0으로 됨
-     */
+    /** @brief 모든 요소를 제거하고, _size를 0으로 만드는 함수입니다 */
     void clear();
 
+    /**
+     * @brief 특정 위치에 요소의 인자를 받아 객체를 생성합니다
+     * @param _ptr 요소를 삽입할 위치
+     * @param args 생성자 인자들
+     * @note 모든 요소를 소멸하고 size가 0으로 됨
+     */
     template <typename ...Args> 
     void emplace(iterator _ptr, Args&&... args);
-    void insert(iterator _ptr, const T& value);
-    void insert(iterator _ptr, T&& value);
-    void erase(iterator _ptr);
 
     /**
-     * @brief 반복자
-     * @return 각 요소에 대한 반복자(포인터)를 반환
+     * @brief 특정 위치에 객체를 삽입합니다
+     * @param _ptr 요소를 삽입할 위치에 대한 Iterator(포인터) 변수
+     * @param value 삽입할 객체 (복사, 이동)
+     * @note 마지막 삽입의 경우 기존 push_back과 동일하게 동작합니다
      */
+    void insert(iterator _ptr, const T& value);
+    void insert(iterator _ptr, T&& value);
+
+    /**
+     * @brief 특정 위치에 객체를 제거합니다
+     * @param _ptr 제거할 요소 위치에 대한 Iterator(포인터) 변수
+     * @details 마지막 요소의 제거인 경우 기존 pop_back과 동일하게 동작합니다
+     */
+    void erase(iterator _ptr);
+
     iterator begin() { return vectorArray; }
     iterator end() { return vectorArray + _size; }
     const_iterator begin() const { return vectorArray; }
@@ -187,18 +178,19 @@ _size(other._size)
 {
   vectorArray = alloc.allocate(_capacity);
 
-  int i=0;
   try
   {
-    for (size_t i=0; i<_size; ++i)
-      new (vectorArray+i) T(other.vectorArray[i]);
+    // 이동이 가능하면 이동, 아니면 복사
+    if constexpr (std::is_nothrow_move_constructible<T>::value)
+      std::uninitialized_move(other.begin(), other.end(), vectorArray);
+    else
+      std::uninitialized_copy(other.begin(), other.end(), vectorArray);
   }
   catch(const std::exception& e)
   {
-    for (; i>=0; --i)
-      std::destroy_at(vectorArray[i]);
     alloc.deallocate(vectorArray, _capacity);
     std::cerr << e.what() << '\n';
+    throw;
   }  
 }
 
@@ -226,17 +218,16 @@ Vector<T>& Vector<T>::operator=(Vector<T> other)
 template <typename T>
 T& Vector<T>::at(size_t index)
 {
-  if (index < 0 || index >= _size) throw std::out_of_range("index 범위가 배열의 범위를 벗어났습니다");
+  if (index >= _size) throw std::out_of_range("index 범위가 배열의 범위를 벗어났습니다");
   return vectorArray[index];
 }
 
 template <typename T>
 const T& Vector<T>::at(size_t index) const
 {
-  if (index < 0 || index >= _size) throw std::out_of_range("index 범위가 배열의 범위를 벗어났습니다");
+  if (index >= _size) throw std::out_of_range("index 범위가 배열의 범위를 벗어났습니다");
   return vectorArray[index];
 }
-
 
 template <typename T>
 void Vector<T>::reserve(size_t newCapacity)
@@ -247,7 +238,11 @@ void Vector<T>::reserve(size_t newCapacity)
 
   try
   {
-    std::uninitialized_move(begin(), end(), newArray);
+    // 이동이 가능하면 이동, 아니면 복사
+    if constexpr (std::is_nothrow_move_constructible<T>::value)
+      std::uninitialized_move(begin(), end(), newArray);
+    else
+      std::uninitialized_copy(begin(), end(), newArray);
   }
   catch(const std::exception& e)
   {
@@ -304,19 +299,17 @@ void Vector<T>::shrink_to_fit()
   T* newArray = alloc.allocate(newCapacity);
 
   // 새로운 메모리 공간에 기존 값을 복사/이동
-  int i=0;
   try
   {
-    for (; i<_size; ++i)
-      new (newArray+i) T(std::move_if_noexcept(vectorArray[i]));
+    std::uninitialized_move(begin(), begin()+_size, newArray);
   }
   catch(const std::exception& e)
   {
-    for (; i>=0; --i) std::destroy_at(newArray+i);
     alloc.deallocate(newArray, newCapacity);
     throw;
   }
   
+  std::destroy(begin(), begin()+_size);
   alloc.deallocate(vectorArray, _capacity);
   _capacity = newCapacity;
   vectorArray = newArray;
@@ -325,8 +318,7 @@ void Vector<T>::shrink_to_fit()
 template <typename T>
 void Vector<T>::clear()
 {
-  for (size_t i=0; i<_size; ++i)
-    std::destroy_at(vectorArray+i);
+  std::destroy(begin(), begin()+_size);
   _size = 0;
 }
 
@@ -340,6 +332,13 @@ void Vector<T>::emplace(iterator _ptr, Args&&... args)
     size_t temp = _ptr - begin();
     resize();
     _ptr = begin() + temp;
+  }
+
+  if (_ptr == end())
+  {
+    new (end()) T(std::forward<Args>(args)...);
+    _size++;
+    return;
   }
 
   // 정의되지 않은 공간 - 맨 뒷 공간에 객체 생성
@@ -366,6 +365,13 @@ void Vector<T>::insert(iterator _ptr, const T& value)
     _ptr = begin() + temp;
   }
 
+  if (_ptr == end())
+  {
+    new (end()) T(std::move(value));
+    _size++;
+    return;
+  }
+
   // 정의되지 않은 공간 - 맨 뒷 공간에 객체 생성
   iterator it = end();
   new (it) T(std::move(*(it-1)));
@@ -390,6 +396,13 @@ void Vector<T>::insert(iterator _ptr, T&& value)
     _ptr = begin() + temp;
   }
 
+  if (_ptr == end())
+  {
+    new (end()) T(std::move(value));
+    _size++;
+    return;
+  }
+
   // 정의되지 않은 공간 - 맨 뒷 공간에 객체 생성
   iterator it = end();
   new (it) T(std::move(*(it-1)));
@@ -407,6 +420,13 @@ template <typename T>
 void Vector<T>::erase(iterator _ptr)
 {
   if(isEmpty()) throw std::out_of_range("Error erase: 소멸할 객체가 없습니다");
+
+  if(_ptr == end()-1)
+  {
+    std::destroy_at(end()-1);
+    _size--;
+    return;
+  }
 
   iterator it = end()-1;
   for (; _ptr != it; ++_ptr)
@@ -432,7 +452,11 @@ void Vector<T>::resize()
   // 3. 새로운 메모리 공간에 기존 값을 이동
   try
   {
-    std::uninitialized_move(begin(), end(), newArray);
+    // 이동이 가능하면 이동, 아니면 복사
+    if constexpr (std::is_nothrow_move_constructible<T>::value)
+      std::uninitialized_move(begin(), end(), newArray);
+    else
+      std::uninitialized_copy(begin(), end(), newArray);
   }
   catch(const std::exception& e)
   {
