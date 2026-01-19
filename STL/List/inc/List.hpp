f59af6b1ecec2c516b208178b7b6b7b04ec5a63f
toproof <kmnlmn123@gmail.com>
2026-01-19 14:32:55 +0900

    feat: 리스트의 삽입, 제거 및 기타 기능 추가
    - push_front 및 push_back 함수 구현
    - pop_front 및 pop_back 함수 구현
    - iterator 구조체 추가 및 일반, 상수, reverse에 대한 begin 및 end 함수 구현
    - insert 및 erase 함수 구현
    - emplace_front 및 emplace_back, emplace 함수 구현
    - erase 함수 구현
    - 일괄 제거하는 remove 함수 구현

--- STL/List/inc/List.hpp
+++ STL/List/inc/List.hpp
@@ -1,6 +1,6 @@
 /**
  * @file List.hpp
- * @date 2026-01-18
+ * @date 2026-01-19
  * @author toproof (kmnlmn123@gmail.com)
  * @brief Raw Memory를 이용한 List 구조의 Class 구현
  */
@@ -18,22 +18,79 @@ template <typename T>
 class List
 {
 private:
-  struct Node 
+  struct Node
   {
-    T node;
+    T value;
     Node* next_node = nullptr;
     Node* pre_node = nullptr;
-    Node(T value) : node(value) {}
+    Node(T value) : value(value) {}
   };
 
-private:
   size_t _capacity;
-
   Node* frontNode = nullptr;
   Node* backNode = nullptr;
 
 public:
-  // o
+  template <typename _ptr, typename _ref>
+  struct ListIterator
+  {
+    _ptr it;
+
+    ListIterator(_ptr _it) : it(_it) {};
+
+    ListIterator<_ptr, _ref>& operator++()    { it = it->next_node; return *this; }
+    ListIterator<_ptr, _ref> operator++(int) { ListIterator<_ptr, _ref> temp = *this; ++*this; return temp; }
+
+    ListIterator<_ptr, _ref>& operator--()    { it = it->pre_node; return *this; }
+    ListIterator<_ptr, _ref> operator--(int) { ListIterator<_ptr, _ref> temp = *this; --*this; return temp; }
+
+    _ref operator*() { return it->value; }
+    
+    bool operator==(const ListIterator<_ptr, _ref>& other) { return this->it == other.it; }
+    bool operator!=(const ListIterator<_ptr, _ref>& other) { return !(*this == other); }
+  };
+
+
+  template <typename _ptr, typename _ref>
+  struct ReverseListIterator
+  {
+    _ptr it;
+    ReverseListIterator(_ptr _it) : it(_it) {};
+
+    ReverseListIterator<_ptr, _ref>& operator++()    { it = it->pre_node; return *this; }
+    ReverseListIterator<_ptr, _ref> operator++(int) { ReverseListIterator<_ptr, _ref> temp = *this; ++*this; return temp; }
+
+    ReverseListIterator<_ptr, _ref>& operator--()    { it = it->next_node; return *this; }
+    ReverseListIterator<_ptr, _ref> operator--(int) { ReverseListIterator<_ptr, _ref> temp = *this; --*this; return temp; }
+
+    _ref operator*() { return it->value; }
+    
+    bool operator==(const ReverseListIterator<_ptr, _ref>& other) { return this->it == other.it; }
+    bool operator!=(const ReverseListIterator<_ptr, _ref>& other) { return !(*this == other); }
+    bool operator>(const ReverseListIterator<_ptr, _ref>& other) { return this->it->value >= other->it->value; }
+  };
+
+  using iterator = ListIterator<Node*, T&>;
+  using const_iterator = ListIterator<const Node*, const T&>;
+  
+  using reverse_iterator = ReverseListIterator<Node*, T&>;
+  using reverse_const_iterator = ReverseListIterator<const Node*, const T&>;
+
+  iterator begin() { return iterator(frontNode); }
+  iterator end() { return iterator(backNode->next_node); }
+  
+  const_iterator cbegin() { return const_iterator(frontNode); }
+  const_iterator cend() { return const_iterator(backNode->next_node); }
+
+  reverse_iterator rbegin() { return reverse_iterator(backNode); }
+  reverse_iterator rend() { return reverse_iterator(frontNode->pre_node); }
+  
+  reverse_const_iterator crbegin() { return reverse_const_iterator(backNode); }
+  reverse_const_iterator crend() { return reverse_const_iterator(frontNode->pre_node); }
+
+
+public:
+
   List() : _capacity(0) {}
   ~List();
 
@@ -41,8 +98,8 @@ public:
   List(List<T>&& other) noexcept;
   List<T>* operator=(List<T> other);
 
-  T& front() { return frontNode->node; }
-  T& back() { return backNode->node; }
+  T& front() { return frontNode->value; }
+  T& back() { return backNode->value; }
   size_t size() { return _capacity; }
 
   // o
@@ -54,40 +111,51 @@ public:
   void push_back(T&& value);
 
   // o
-  template <typename ...Args>
-  void emplcae_front(Args&& ...args);
+  template <typename... Args>
+  void emplace_front(Args&& ...args);
 
   // o
-  template <typename ...Args>
-  void emplcae_back(Args&& ...args);
+  template <typename... Args>
+  void emplace_back(Args&& ...args);
 
+  // o
   void pop_front();
   void pop_back();
 
-  void insert();
-  void emplace();
+  void insert(iterator& _iterator, const T& value);
+  void insert(iterator& _iterator, T&& value);
 
-  void erase();
-  void remove();
+  template <typename... Args>
+  void emplace(iterator& _iterator, Args&& ...args);
+
+  void erase(iterator& _iterator);
+
+  void remove(const T& value);
 
 
   void printList()
   {
     Node* current = frontNode;
     int i=1;
+    std::cout << "size (" << size() << ") - ";
     while (current != nullptr)
     {
-      std::cout << '[' << i++ << "] : " << current->node << " → "; 
+      std::cout << '[' << i << "] : " << current->value << (current != backNode ? " → " : ""); 
       current = current->next_node;
+      i++;
     }
     std::cout << "\n"; 
-    //std::cout << '[' << i << "] : " << current->node << "\n"; 
   }
 };
 
 template <typename T>
 List<T>::~List()
 {
+  if (frontNode == nullptr)
+  {
+    return;
+  }
+
   std::allocator<Node> alloc;
 
   Node* deleteNode = frontNode;
@@ -103,13 +171,47 @@ List<T>::~List()
 }
 
 
+template <typename T>
+List<T>::List(const List<T>& other)
+{
+  std::allocator<Node> alloc;
+
+  try
+  {
+    /* code */
+  }
+  catch(const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+  
+  
+}
+
+template <typename T>
+List<T>::List(List<T>&& other) noexcept : frontNode(other.frontNode), backNode(other.backNode), _capacity(other._capacity)
+{
+  other.frontNode = nullptr;
+  other.backNode = nullptr;
+  other._capacity = 0;
+}
+
+template <typename T>
+List<T>* List<T>::operator=(List<T> other)
+{
+  std::swap(frontNode, other.frontNode);
+  std::swap(backNode, other.backNode);
+  std::swap(_capacity, other._capacity);
+  return *this;
+}
+
 
 template <typename T>
 void List<T>::push_front(const T& value)
 {
   std::allocator<Node> alloc;
 
-  if (_capacity == 0)
+  if (frontNode == nullptr)
   {
     frontNode = alloc.allocate(1);
     new (frontNode) Node(value);
@@ -120,10 +222,9 @@ void List<T>::push_front(const T& value)
     Node* node = alloc.allocate(1);
     new (node) Node(value);
 
-
     node->next_node = frontNode;
+    frontNode->pre_node = node;
     frontNode = node;
-    
   }
   
   _capacity++;
@@ -133,7 +234,7 @@ void List<T>::push_front(T&& value)
 {
   std::allocator<Node> alloc;
 
-  if (_capacity == 0)
+  if (frontNode == nullptr)
   {
     frontNode = alloc.allocate(1);
     new (frontNode) Node(std::move(value));
@@ -145,6 +246,7 @@ void List<T>::push_front(T&& value)
     new (node) Node(std::move(value));
 
     node->next_node = frontNode;
+    frontNode->pre_node = node;
     frontNode = node;
   }
   
@@ -157,7 +259,7 @@ void List<T>::push_back(const T& value)
 {
   std::allocator<Node> alloc;
 
-  if (_capacity == 0)
+  if (backNode == nullptr)
   {
     frontNode = alloc.allocate(1);
     new (frontNode) Node(value);
@@ -167,6 +269,7 @@ void List<T>::push_back(const T& value)
   {
     Node* node = alloc.allocate(1);
     new (node) Node(value);
+    node->pre_node = backNode;
     backNode->next_node = node;
     backNode = node;
   }
@@ -178,7 +281,7 @@ void List<T>::push_back(T&& value)
 {
   std::allocator<Node> alloc;
 
-  if (_capacity == 0)
+  if (backNode == nullptr)
   {
     frontNode = alloc.allocate(1);
     new (frontNode) Node(std::move(value));
@@ -188,6 +291,7 @@ void List<T>::push_back(T&& value)
   {
     Node* node = alloc.allocate(1);
     new (node) Node(std::move(value));
+    node->pre_node = backNode;
     backNode->next_node = node;
     backNode = node;
   }
@@ -199,23 +303,24 @@ void List<T>::push_back(T&& value)
 
 // emplace 삽입
 template <typename T>
-template <typename ...Args>
-void List<T>::emplcae_front(Args&& ...args)
+template <typename... Args>
+void List<T>::emplace_front(Args&& ...args)
 {
   std::allocator<Node> alloc;
 
-  if (_capacity == 0)
+  if (frontNode == nullptr)
   {
     frontNode = alloc.allocate(1);
-    new (frontNode) Node(std::forward<T>(args)...);
+    new (frontNode) Node({std::forward<Args>(args)...});
     backNode = frontNode;
   }
   else
   {
     Node* node = alloc.allocate(1);
-    new (node) Node(std::forward<T>(args)...);
+    new (node) Node({std::forward<Args>(args)...});
 
     node->next_node = frontNode;
+    frontNode->pre_node = node;
     frontNode = node;
   }
   
@@ -223,21 +328,22 @@ void List<T>::emplcae_front(Args&& ...args)
 }
 
 template <typename T>
-template <typename ...Args>
-void List<T>::emplcae_back(Args&& ...args)
+template <typename... Args>
+void List<T>::emplace_back(Args&& ...args)
 {
   std::allocator<Node> alloc;
 
-  if (_capacity == 0)
+  if (backNode == nullptr)
   {
     frontNode = alloc.allocate(1);
-    new (frontNode) Node(std::forward<T>(args)...);
+    new (frontNode) Node({std::forward<Args>(args)...});
     backNode = frontNode;
   }
   else
   {
     Node* node = alloc.allocate(1);
-    new (node) Node(std::forward<T>(args)...);
+    new (node) Node({std::forward<Args>(args)...});
+    node->pre_node = backNode;
     backNode->next_node = node;
     backNode = node;
   }
@@ -251,8 +357,20 @@ void List<T>::pop_front()
 {
   std::allocator<Node> alloc;
   
-  Node* deleteNode = frontNode;
-  frontNode = deleteNode->next_node;
+  if (frontNode->next_node == nullptr)
+  {
+    std::destroy_at(frontNode);
+    alloc.deallocate(frontNode, 1);
+    frontNode = nullptr;
+    backNode = nullptr;
+    _capacity--;
+    return;
+  }
+
+  Node* deleteNode;
+  frontNode = frontNode->next_node;
+  deleteNode = frontNode->pre_node;
+  frontNode->pre_node = nullptr;
 
   std::destroy_at(deleteNode);
   alloc.deallocate(deleteNode, 1);
@@ -264,9 +382,194 @@ template <typename T>
 void List<T>::pop_back()
 {
   std::allocator<Node> alloc;
-  std::destroy_at(backNode);
-  alloc.deallocate(backNode, 1);
-  backNode = nullptr;
+  
+  if (backNode->pre_node == nullptr)
+  {
+    std::destroy_at(backNode);
+    alloc.deallocate(backNode, 1);
+    frontNode = nullptr;
+    backNode = nullptr;
+    _capacity--;
+    return;
+  }
+
+  Node* deleteNode;
+  backNode = backNode->pre_node;
+  deleteNode = backNode->next_node;
+  backNode->next_node = nullptr;
+
+  std::destroy_at(deleteNode);
+  alloc.deallocate(deleteNode, 1);
 
   _capacity--;
+}
+
+template <typename T>
+void List<T>::insert(iterator& _iterator, const T& value)
+{
+  // 첫번째 위치라면 push_front
+  if (frontNode == _iterator.it)
+  {
+    push_front(value);
+    return;
+  }
+  
+  // 마지막 위치라면 push_back
+  if (backNode == _iterator.it)
+  {
+    push_back(value);
+    return;
+  }
+
+  Node* currentNode = _iterator.it;
+  Node* preNode = currentNode->pre_node;
+
+  std::allocator<Node> alloc;
+  Node* node = alloc.allocate(1);
+  new (node) Node(value);
+
+  preNode->next_node = node;
+
+  node->pre_node = preNode;
+  node->next_node = currentNode;
+
+  currentNode->pre_node = node;
+  _capacity++;
+}
+
+template <typename T>
+void List<T>::insert(iterator& _iterator, T&& value)
+{
+  // 첫번째 위치라면 push_front
+  if (frontNode == _iterator.it)
+  {
+    push_front(value);
+    return;
+  }
+  
+  // 마지막 위치라면 push_back
+  if (backNode == _iterator.it)
+  {
+    push_back(value);
+    return;
+  }
+
+  Node* currentNode = _iterator.it;
+  Node* preNode = currentNode->pre_node;
+
+  std::allocator<Node> alloc;
+  Node* node = alloc.allocate(1);
+  new (node) Node(std::move(value));
+
+  preNode->next_node = node;
+
+  node->pre_node = preNode;
+  node->next_node = currentNode;
+
+  currentNode->pre_node = node;
+  _capacity++;
+}
+
+
+template <typename T>
+template <typename... Args>
+void List<T>::emplace(iterator& _iterator, Args&& ...args)
+{
+  // 첫번째 위치라면 push_front
+  if (frontNode == _iterator.it)
+  {
+    emplace_front(std::forward<Args>(args)...);
+    return;
+  }
+  
+  // 마지막 위치라면 push_back
+  if (backNode == _iterator.it)
+  {
+    emplace_back(std::forward<Args>(args)...);
+    return;
+  }
+
+  Node* currentNode = _iterator.it;
+  Node* preNode = currentNode->pre_node;
+
+  std::allocator<Node> alloc;
+  Node* node = alloc.allocate(1);
+  new (node) Node({std::forward<Args>(args)...});
+
+  preNode->next_node = node;
+
+  node->pre_node = preNode;
+  node->next_node = currentNode;
+
+  currentNode->pre_node = node;
+  _capacity++;
+}
+
+
+template <typename T>
+void List<T>::erase(iterator& _iterator)
+{
+  if (frontNode == _iterator.it)
+  {
+    pop_front();
+    return;
+  }
+
+  if (backNode == _iterator.it)
+  {
+    pop_back();
+    return;
+  }
+
+  Node* deleteNode = _iterator.it;
+
+  Node* preNode = deleteNode->pre_node;
+  Node* nextNode = deleteNode->next_node;
+
+  preNode->next_node = nextNode;
+  nextNode->pre_node = preNode;
+
+  std::allocator<Node> alloc;
+  std::destroy_at(deleteNode);
+  alloc.deallocate(deleteNode, 1);
+
+  _capacity--;
+}
+
+template <typename T>
+void List<T>::remove(const T& value)
+{
+  Node* currentNode = frontNode;
+
+  std::allocator<Node> alloc;
+
+  Node* deleteNode;
+  Node* preNode;
+  Node* nextNode;
+
+  while (currentNode != backNode->next_node)
+  {
+    if (currentNode->value == value)
+    {
+      if (currentNode == frontNode) 
+        pop_front();
+      else if (currentNode == backNode) 
+        pop_back();
+      else
+      {
+        deleteNode = currentNode;
+        preNode = deleteNode->pre_node;
+        nextNode = deleteNode->next_node;
+
+        preNode->next_node = nextNode;
+        nextNode->pre_node = preNode;
+
+        std::destroy_at(deleteNode);
+        alloc.deallocate(deleteNode, 1);
+        _capacity--;
+      }
+    }
+
+    currentNode = currentNode->next_node;
+  }
 }
