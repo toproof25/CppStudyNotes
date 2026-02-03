2a975ba702d2b6164b6a3f59b642aad2a56aa18c
toproof <kmnlmn123gmail.com>
2026-02-04 00:04:24 +0900

    feat: Vector를 이용한 Heap 구조 클래스 구현
    - 기존 Vector Class를 기반으로 Heap 자료구조 구현
    - - zero overhead, Rule of Zero, RAII 원칙 준수
    - Heap 삽입, 삭제 시 우선순위에 따라 정렬 기능 추가
    - template _g 를 사용하여 최소 힙, 최대 힙을 구현하였으며, constexpr 키워드를 통해 컴파일 타임에 우선순위 결정

--- STL/Heap/inc/Heap.hpp
+++ STL/Heap/inc/Heap.hpp
@@ -1,29 +1,65 @@
+/**
+ * @file Heap.hpp
+ * @date 2026-02-03
+ * @author toproof(kmnlmn123@gmail.com)
+ * @brief
+ * - 이전에 구현한 Vector Class를 이용하여 Heap Class의 요소 저장을 관리하여 Zero-Overhead 원칙을 준수
+ * - Vector Class에서 생성과 소멸의 책임을 가져 RAII와 예외 안정성을 보장함
+ * - 배열 구조로 Heap을 구현하여 오버헤드가 적고 빠근 접근이 가능하다
+ * 
+ */
+
+
+
 #pragma once
 
 #include <iostream>
 #include <memory>
 #include "../../Vector/inc/Vector.hpp"
 
-
-// 0-based index 기준
-// 부모 (i - 1) / 2 
-// 왼쪽 자식  2*i + 1 
-// 오른쪽 자식 2*i + 2
-
-template <typename T>
+/**
+ * @brief Heap 구조로 저장되는 Class
+ * @tparam T 저장되는 객체의 타입
+ * @tparam _g 0이면 최소값이 우선순위, 아닌 경우 최대값이 우선순위
+ */
+template <typename T, int _g = 0>
 class Heap
 {
 
 private:
   Vector<T> vector;
-  T* rootNode = nullptr;
 
 public:
-  //void push(T&& data);
+
+  /**
+   * @brief Heap에 우선순위를 고려하여 data를 삽입합니다
+   * @param data 저장할 객체
+   * @note 
+   * 1. Vector 맨 끝에 data를 삽입합니다
+   * 2. 해당 위치부터 부모와 값을 비교하여 swap을 반복합니다
+   * 3. root이거나 더 이상 부모보다 우선순위가 아닐 경우 종료합니다
+   */
+  void push(T&& data);
   void push(const T& data);
+
+  /**
+   * @brief 가장 높은 우선순위 요소를 제거하며, 내부 요소의 우선순위를 재정렬합니다
+   * @note 
+   * 1. 최상단 우선순위 요소를 제거합니다
+   * 2. 마지막 요소를 root로 만든다.
+   * 2. root 요소부터 자식들 값과 우선순위를 비교하여 swap을 반복합니다
+   * 3. 마지막 index거나 자식보다 우선순위가 낮은 경우 종료합니다
+   */
   void pop();
+  
+  T& top() { return vector.front(); }
+  const T& top() const { return vector.front(); }
 
+  size_t size() { return vector.size(); }
+  bool empty() { return vector.size() == 0; }
 
+  
+  /** @brief 테스트용 출력 함수 */
   void printHeap()
   {
     std::cout << "Heap Class List("<< vector.size() <<"): ";
@@ -33,92 +69,144 @@ public:
   }
 };
 
-
-template <typename T>
-void Heap<T>::push(const T& data)
+template <typename T, int _g>
+void Heap<T, _g>::push(T&& data)
 {
-  /*
-  - 삽입 시 항상 자식 노드가 숫자가 더 커야한다
-  - 맨 아래층에서 왼쪽 부터 삽입한다
+  vector.push_back(std::forward<T>(data));
+  
+  size_t currentIndex = vector.size()-1;
+  size_t parantIndex = (currentIndex - 1) / 2;
 
-  1. 일단 vector에 삽입한다 (알아서 resize함)
-  2. 이후 부모와 값을 비교하여 교체한다
-  3. 교체한 후 부모의 값보다 작은 경우에만 교체를 멈춘다
-  */
+  if constexpr (_g == 0)
+  {
+    while(currentIndex > 0 && vector[currentIndex] < vector[parantIndex])
+    {
+      T temp = std::move(vector[currentIndex]);
+      vector[currentIndex] = std::move(vector[parantIndex]);
+      vector[parantIndex] = std::move(temp);
+      
+      currentIndex = parantIndex;
+      parantIndex = (currentIndex - 1) / 2;
+    }
+  }
+  else
+  {
+    while(currentIndex > 0 && vector[currentIndex] > vector[parantIndex])
+    {
+      T temp = std::move(vector[currentIndex]);
+      vector[currentIndex] = std::move(vector[parantIndex]);
+      vector[parantIndex] = std::move(temp);
+      
+      currentIndex = parantIndex;
+      parantIndex = (currentIndex - 1) / 2;
+    }
+  }
 
+
+}
+
+
+template <typename T, int _g>
+void Heap<T, _g>::push(const T& data)
+{
   vector.push_back(data);
   
   size_t currentIndex = vector.size()-1;
   size_t parantIndex = (currentIndex - 1) / 2;
 
-  // 부모보다 값이 큰 경우
-  while(currentIndex > 0 && vector[currentIndex] < vector[parantIndex])
+  if constexpr (_g == 0)
   {
-    T temp = std::move(vector[currentIndex]);
-    vector[currentIndex] = std::move(vector[parantIndex]);
-    vector[parantIndex] = std::move(temp);
-    
-    currentIndex = parantIndex;
-    parantIndex = (currentIndex - 1) / 2;
+    while(currentIndex > 0 && vector[currentIndex] < vector[parantIndex])
+    {
+      T temp = std::move(vector[currentIndex]);
+      vector[currentIndex] = std::move(vector[parantIndex]);
+      vector[parantIndex] = std::move(temp);
+      
+      currentIndex = parantIndex;
+      parantIndex = (currentIndex - 1) / 2;
+    }
   }
-  
+  else
+  {
+    while(currentIndex > 0 && vector[currentIndex] > vector[parantIndex])
+    {
+      T temp = std::move(vector[currentIndex]);
+      vector[currentIndex] = std::move(vector[parantIndex]);
+      vector[parantIndex] = std::move(temp);
+      
+      currentIndex = parantIndex;
+      parantIndex = (currentIndex - 1) / 2;
+    }
+  }
+
 }
 
-template <typename T>
-void Heap<T>::pop()
+template <typename T, int _g>
+void Heap<T, _g>::pop()
 {
   if (vector.size() == 0) return;
-  if (vector.size() == 1) {
-    vector.pop_back();
-    return;
-  }
+  if (vector.size() == 1) { vector.pop_back(); return; }
 
-  /*
-  1. 가장 위에 있는 노드를 제거한다
-  2. 마지막 레벨에 가장 우측 노드를 root로 설정한다
-  3. root의 자식과 값을 비교하여 교체한다
-  */
+  size_t vSize = vector.size()-1;
 
-  // root 노드와 마지막 노드를 교체한 후 제거
-  T temp = std::move(vector.back()); ///< 포인터 타입을 저장하는 경우 메모리 누수 발생..?
-  vector.back() = std::move(vector.front());
-  vector.front() = std::move(temp);
+  // 맨 앞 요소와 맨 뒤 요소 위치를 바꾼 후 맨 뒤 요소를 제거합니다
+  T temp = std::move(vector[vSize]);
+  vector[vSize] = std::move(vector[0]);
+  vector[0] = std::move(temp);
   vector.pop_back();
 
   size_t currentIndex = 0;
   size_t l_child = 2 * currentIndex + 1;
   size_t r_child = 2 * currentIndex + 2;
   size_t min_child;
-  size_t vSize = vector.size()-1;
+  vSize = vector.size()-1;
 
-  if (vSize == currentIndex)
-    return;
-  else if (vSize < r_child)
-    min_child = l_child;
-  else
-    min_child = (vector[l_child] < vector[r_child]) ? l_child : r_child;
 
-  // 부모 노드가 자식 노드보다 큰 경우
-  while (vector[currentIndex] > vector[min_child] )
+  if constexpr (_g == 0)
   {
-    // 부모 노드와 교체
-    temp = std::move(vector[min_child]);
-    vector[min_child] = std::move(vector[currentIndex]);
-    vector[currentIndex] = std::move(temp);
-
-    currentIndex = min_child;
-    l_child = 2 * currentIndex + 1;
-    r_child = 2 * currentIndex + 2;
-    
-    // 두 자식 중 가장 작은 노드를 구함
-
-    ///****여기에 문제 있음 집에서 수정 */
-    if (vSize == currentIndex)
-      continue;
-    else if (vSize < r_child)
+    if (vSize < r_child)
       min_child = l_child;
     else
       min_child = (vector[l_child] < vector[r_child]) ? l_child : r_child;
+
+    while (vSize >= min_child && vector[currentIndex] > vector[min_child] )
+    {
+      temp = std::move(vector[min_child]);
+      vector[min_child] = std::move(vector[currentIndex]);
+      vector[currentIndex] = std::move(temp);
+
+      currentIndex = min_child;
+      l_child = 2 * currentIndex + 1;
+      r_child = 2 * currentIndex + 2;
+      
+      if (vSize < r_child)
+        min_child = l_child;
+      else
+        min_child = (vector[l_child] < vector[r_child]) ? l_child : r_child;
+    }
+  }
+  else
+  {
+    if (vSize < r_child)
+      min_child = l_child;
+    else
+      min_child = (vector[l_child] > vector[r_child]) ? l_child : r_child;
+
+    while (vSize >= min_child && vector[currentIndex] < vector[min_child] )
+    {
+      temp = std::move(vector[min_child]);
+      vector[min_child] = std::move(vector[currentIndex]);
+      vector[currentIndex] = std::move(temp);
+
+      currentIndex = min_child;
+      l_child = 2 * currentIndex + 1;
+      r_child = 2 * currentIndex + 2;
+      
+      if (vSize < r_child)
+        min_child = l_child;
+      else
+        min_child = (vector[l_child] < vector[r_child]) ? l_child : r_child;
+    }
   }
 
 
